2a3a4c910a44a5ca194c3d1ae4cde813a9e301ea
mcereal <mpbcurtis@gmail.com>
2021-01-14 13:53:59 -0500

    working on put route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,7 +17,18 @@ router.get("/api/workouts", async (req, res) => {
     res.json(error);
   }
 });
-router.put("/api/workouts", ({ body }, res) => {});
+router.put("/api/workouts", async ({ body, params }, res) => {
+  try {
+    const findById = await Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    );
+    res.json(findById);
+  } catch (error) {
+    res.json(error);
+  }
+});
 router.get("/api/workouts/range", ({ body }, res) => {});
 
 module.exports = router;
