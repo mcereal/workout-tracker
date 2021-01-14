98120ec911dc03bc8ec6dd5b2df07b05530fc1a2
mcereal <mpbcurtis@gmail.com>
2021-01-14 12:15:14 -0500

    trying to get transaction example working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,14 @@
-module.exports = (app) => {
-  app.get("/api/workouts", (req, res) => {
-    console.log("test");
-  });
-};
+const router = require("express").Router();
+const Transaction = require("../models/workout.js");
+
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
