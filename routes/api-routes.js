6989da649bd33c24d1d2baf4c27e7072652a1e11
mcereal <mpbcurtis@gmail.com>
2021-01-14 13:43:53 -0500

    get and post routes working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create({})
-    .then((data) => res.json(data))
-    .catch((err) => {
-      res.json(err);
-    });
+router.post("/api/workouts", async ({ body }, res) => {
+  try {
+    const workout = await Workout.create(body);
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
 });
-router.get("/api/workouts", ({ body }, res) => {});
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const workout = await Workout.find();
+    res.json(workout);
+  } catch (error) {
+    res.json(error);
+  }
+});
 router.put("/api/workouts", ({ body }, res) => {});
 router.get("/api/workouts/range", ({ body }, res) => {});
 
